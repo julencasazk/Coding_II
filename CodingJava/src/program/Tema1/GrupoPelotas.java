bf416f33e57b0b28eadaca41e362b3e64b1c7fd1
julencasazk <julencasazk@gmail.com>
2020-02-22 17:24:40 +0100

    GrupoPelotas update

--- CodingJava/src/program/Tema1/GrupoPelotas.java
+++ CodingJava/src/program/Tema1/GrupoPelotas.java
@@ -1,24 +1,41 @@
 package program.Tema1;
 
-import java.lang.reflect.Array;
 
 /**
  * Clase para manejar un grupo de pelotas
  */
 public class GrupoPelotas {
 
-    Pelota[] arrayPelotas = {
-        new Pelota(), new Pelota(), new Pelota(), new Pelota()
-        // TODO
-    };
+    int grupoTamanyo;
+    Pelota pelotaArray[] = new Pelota [grupoTamanyo];
 
+
+    public GrupoPelotas( int tamanyo ) {
+        grupoTamanyo = tamanyo;
+    }
+
+    
     /**
      * Meter nueva pelota al grupo
      * @param pelota Nueva pelota a meter en el grupo
      * @return True si se ha metido en la pelota, False si no es posible (grupo lleno)
      */
     public boolean addPelota( Pelota pelota ) {
+
+        Pelota nuevoGrupo[] = new Pelota [grupoTamanyo + 1];
+        for (int i = 0; i < nuevoGrupo.length; i++) {
+            nuevoGrupo[i] = pelotaArray [i];
+        }
+        nuevoGrupo[nuevoGrupo.length - 1] = pelota;
+        pelotaArray = nuevoGrupo;
+        ++grupoTamanyo;
+        return true;
+
         // TODO
+        // ¿Como pongo límite a los items que puedo meter?
+        // Ahora mismo creo un array con length + 1 para poder meter una Pelota nueva
+        // Por lo que NUNCA va a devolver FALSE
+
         
     }
 
@@ -27,6 +44,14 @@ public class GrupoPelotas {
      * @param numPelota Indice de la pelota a quitar
      */
     public void rmPelota( int numPelota ) {
+        Pelota nuevoGrupo[] = new Pelota [grupoTamanyo - 1];
+        for (int i = 0; i < pelotaArray.length; i++) {
+            if (i != numPelota) {
+                nuevoGrupo[i] = pelotaArray[i];
+            }
+        }
+        pelotaArray = nuevoGrupo;
+        --grupoTamanyo;
         // TODO
 
         
@@ -38,7 +63,7 @@ public class GrupoPelotas {
      * @return El objeto Pelota correspondiente al indice numPelota (No la quita del grupo)
      */
     public Pelota getPelota( int numPelota ) {
-        // TODO
+        return pelotaArray[numPelota];
         
     }
 
@@ -48,7 +73,13 @@ public class GrupoPelotas {
      * @return Posicion de la pelota deseada
      */
     public int buscaPelota( Pelota pelota ) {
+        for (int i = 0; i < pelotaArray.length; i++) {
+            if (pelotaArray[i].equals(pelota)) {
+                return i;
+            }
+        }
         // TODO
+        // Aparece que no devuelve un int, pero la var "i" es un int
         
     }
 
@@ -58,6 +89,9 @@ public class GrupoPelotas {
      * @param v Ventana grafica donde dibujar las pelotas
      */
     public void dibujaPelota( VentanaGrafica v ) {
+        for (int i = 0; i < pelotaArray.length; i++) {
+            pelotaArray[i].dibujar(v);
+        }
         // TODO
 
     }
