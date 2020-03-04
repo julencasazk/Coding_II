bab3d871de653b368ac48b2b4a7d80dfcdede20a
julencasazk <julencasazk@gmail.com>
2020-03-04 15:00:48 +0100

    Added now file for testing

---
+++ CodingJava/src/program/Tema1/Pruebas.java
@@ -0,0 +1,23 @@
+package program.Tema1;
+
+/**
+ * Pruebas
+ */
+public class Pruebas {
+
+    public static void main(String[] args) { 
+
+        int bruh = 0;
+
+        while (bruh != 1) {
+            System.out.println(bruh);
+            bruh++;
+        }
+
+        // do {
+        //     System.out.println(bruh);
+        //     bruh++;
+        // } while (bruh != 1);
+        
+    }
+}
